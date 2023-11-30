b67618fdbdc2f4e14a2ae1737d947900c057919f
Dmitry <budilovdmi@gmail.com>
2023-11-30 22:58:44 +1200

    complited weather_api_service

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,5 @@
 class CantGetCoordinates(Exception):
     """Программа не может получить координаты устройства."""
+
+class ApiServiceError(Exception):
+    """Программа не может обратиться к серверу"""
